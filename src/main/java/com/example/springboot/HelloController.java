c40e7e626bace5ff93e80701cc0309b14a2ed5b3
Stuart Blackburn <blackburns@vmware.com>
2024-09-06 21:19:00 -0400

    dev

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzu! - version 6";
+		return "Greetings from Spring Boot + Tanzu! - version 7";
 	}
 
 }
